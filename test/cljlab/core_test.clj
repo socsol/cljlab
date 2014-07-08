a179b439e886a48e1bccf1c2f62b392a2f994595
Alastair Pharo <asppsa@gmail.com>
2014-07-08 23:05:42 +1000

    Half-finished changes to tests

--- test/cljlab/core_test.clj
+++ test/cljlab/core_test.clj
@@ -21,42 +21,89 @@
 
 (tabular
  (facts
-  (fact "`open` returns something"
-        (cl/open :type ?type) => truthy)
+  (with-state-changes [(after :facts (if-not (nil? @lab) (cl/exit @lab)))]
 
-  (fact "`exit` can be called many times without issue"
-        (doto (cl/open :type ?type)
-              (cl/exit)
-              (cl/exit)
-              (cl/exit)) =not=> (throws java.lang.Exception))
+    (fact "`open` returns something"
+          (cl/open :type ?type) => truthy)
 
-  (fact "`disconnect` can be called many times without issue"
-        (doto (cl/open :type ?type)
-              (cl/disconnect)
-              (cl/disconnect)
-              (cl/disconnect)) =not=> (throws java.lang.Exception))
+    (fact "`exit` can be called many times without issue"
+          (doto (cl/open :type ?type)
+            (cl/exit)
+            (cl/exit)
+            (cl/exit)) =not=> (throws java.lang.Exception))
 
-  (facts "about the lab"
-         (with-state-changes [(before :facts (reset! lab (cl/open :type ?type)))]
+    (fact "`disconnect` can be called many times without issue"
+          (doto (cl/open :type ?type)
+            (cl/disconnect)
+            (cl/disconnect)
+            (cl/disconnect)) =not=> (throws java.lang.Exception))
 
-           (fact "type is correct"
-                 (@lab :type) => ?type)
+    (facts "about the lab"
+           (with-state-changes [(before :facts (reset! lab (cl/open :type ?type)))
+                                (after :facts (cl/exit @lab))]
 
-           (fact "handle is a future"
-                 (is (future? (@lab :handle))))
+             (fact "type is correct"
+                   (@lab :type) => ?type)
 
-           (facts "about `open?`"
-                  (fact "initially true"
-                        (cl/open? @lab) => true)
+             (fact "handle is a future"
+                   (future? (@lab :handle)) => truthy)
 
-                  (fact "false after exit"
-                        (do
-                          (cl/exit @lab)
-                          (cl/open? @lab)) => false)
+             (facts "about `open?`"
+                    (fact "initially true"
+                          (cl/open? @lab) => true)
 
-                  (fact "false after disconnect"
-                        (do
-                          (cl/disconnect @lab)
-                          (cl/open? @lab)) => false)))))
+                    (fact "false after exit"
+                          (do
+                            (cl/exit @lab)
+                            (cl/open? @lab)) => false)
+
+                    (fact "false after disconnect"
+                          (do
+                            (cl/disconnect @lab)
+                            (cl/open? @lab)) => false))
+
+             (facts "about `eval`"
+                    (fact "returns nil on success"
+                          (cl/eval @lab "x = 1;") => nil
+                          (cl/eval @lab "y = x^2;") => nil)
+
+                    (fact "throws an exception if the input is invalid"
+                          (cl/eval @lab "x = $@#$@#$;") => (throws Exception)))
+
+             (facts "about `get-basic`"
+                    (facts "for numeric arrays"
+                           (fact "returns an array of doubles"
+                                 (do
+                                   (cl/eval @lab "x = [1,2,3];")
+                                   (cl/get-basic @lab "x")) => #(= (type %) (Class/forName "[D")))
+
+                           (tabular
+                            (fact "returns the correct elements"
+                                  (do
+                                    (cl/eval @lab ?code)
+                                    (-> @lab
+                                        (cl/get-basic ?var)
+                                        (seq))) => ?values)
+
+                            ?code               ?var ?values
+                            "a = [1,2,3,4,5];"  "a"  (list 1.0 2.0 3.0 4.0 5.0)
+                            "b = 1:0.5:25;"     "b"  (range 1.0 25.5 0.5)))
+
+                    (facts "for strings"
+                           (fact "returns a string"
+                                 (do
+                                   (cl/eval @lab "x = 'testing 1 2 3';")
+                                   (cl/get-basic @lab "x")) => string?)
+
+                           (tabular
+                            (fact "returns the correct elements"
+                                  (do
+                                    (cl/eval @lab ?code)
+                                    (cl/get-basic @lab ?var)) => ?str)
+
+                            ?code               ?var ?str
+                            "a = 'test1';"      "a"  "test1"
+                            "b = 'test2\\n';"   "b"  "test2\\n"
+                            "c = 'test3\\\\n';" "c"  "test3\\\\n")))))))
 
  ?type :matlab :octave)
