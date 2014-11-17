dc96ad03c43786ede0e8ade3a7fc618663532935
Alastair Pharo <asppsa@gmail.com>
2014-11-17 13:55:26 +1100

    version bump and doc update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljlab "0.2.0"
+(defproject cljlab "0.2.1"
   :description "Interface to MATLAB and GNU Octave from Clojure"
   :url "https://github.com/socsol/cljlab"
   :license {:name "Apache Licence, Version 2.0"
@@ -9,4 +9,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [matlabcontrol/matlabcontrol "4.1.0"]
                  [dk.ange/javaoctave "0.6.4"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
